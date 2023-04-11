b9318e0bc7e041c80df9f7acd2740ff2df1a1e9c
Oerd Bej <oerdbej@gmail.com>
2023-04-11 20:21:16 +0200

    adding conditional using the prop type library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
+import Button from "./components/Button";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <div>
+        <Button success primary rounded outline>
+          Click Me
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          Buy Me
+        </Button>
+      </div>
+      <div>
+        <Button warning>See Deal</Button>
+      </div>
+      <div>
+        <Button>What</Button>
+      </div>
+      <div>
+        <Button>Open Source</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Button = ({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+}) => {
+  return <button>{children}</button>;
+};
+
+Button.propTypes = {
+  checkVariationValue: ({
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    outline,
+    rounded,
+  }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!warning) +
+      Number(!!success) +
+      Number(!!danger);
+
+    if (count > 1) {
+      return new Error(
+        "Only primary, secondary, warning, success and danger are allowed"
+      );
+    }
+  },
+};
+
+export default Button;
