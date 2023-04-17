bfb744b13ee098d23aa1fb75419d98a40ad8e91f
Oerd Bej <oerdbej@gmail.com>
2023-04-17 18:09:43 +0200

    accordion component works well

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,21 +1,46 @@
 import React from 'react';
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go';
+
 import { useState } from 'react';
 
 const Accordion = ({ items }) => {
-    const [expandedIndex, setExpandedIndex] = useState(0);
+    const [expandedIndex, setExpandedIndex] = useState(-1);
 
-    // loop from all data props
+    const handleClick = (nextIndex) => {
+        if (expandedIndex === nextIndex) {
+            setExpandedIndex(-1);
+        } else {
+            setExpandedIndex(nextIndex);
+        }
+    };
+
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
 
+        const icon = (
+            <span className='text-4xl'>
+                {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+            </span>
+        );
+
         return (
             <div key={item.id}>
-                <div>{item.label}</div>
-                <div>{isExpanded && <div>{item.content} </div>}</div>
+                {/* 1 for each object = totally separate functions. */}
+                <div
+                    className='flex justify-between p-3 bggray-50 border-b items-center cursor-pointer'
+                    onClick={() => handleClick(index)}>
+                    {item.label}
+                    {icon}
+                </div>
+                <div>
+                    {isExpanded && (
+                        <div className='border-b p-5'>{item.content} </div>
+                    )}
+                </div>
             </div>
         );
     });
-    return <div>{renderedItems}</div>;
+    return <div className='border-x border-t rounded'>{renderedItems}</div>;
 };
 
 export default Accordion;
