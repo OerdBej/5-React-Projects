a65650a4984d91e6d24bb828a334a354c0bd872f
oerd bej <oerdbej@gmail.com>
2024-07-19 21:02:19 +0200

    logic works when hovered or clicked it will fill up

--- Rating/src/components/StarRating.jsx
+++ Rating/src/components/StarRating.jsx
@@ -1,28 +1,46 @@
 import React, { useState } from 'react';
 import { FaStar } from 'react-icons/fa';
+import '../components/style.css';
 
 const StarRating = ({ stars }) => {
   const [starRating, setStarRating] = useState(0);
   const [hover, setHover] = useState(0);
 
-  function handleClick() {
-    console.log('blah');
+  function handleClick(index) {
+    setStarRating(index);
+  }
+
+  function handleMouseEnter(index) {
+    setHover(index);
+  }
+
+  function handleMouseLeave(index) {
+    setHover(starRating);
+    setStarRating(index);
   }
 
   return (
-    <div className='star-rating'>
-      {/* creating the stars in converting them into arrys wo we keep track*/}
-
-      {[...Array(stars)].map((_, index) => {
-        return (
-          <FaStar
-            key={index}
-            onClick={handleClick}
-            onMouseLeave={[]}
-            onMouseEnter={[]}
-          />
-        );
-      })}
+    <div className='center-container'>
+      <h3 className='pastel-text'>slow coding</h3>
+      <div className='star-rating'>
+        {[...Array(stars)].map((_, index) => {
+          index += 1;
+          return (
+            <FaStar
+              className={
+                index <= (starRating || hover)
+                  ? 'star active'
+                  : 'star inactive'
+              }
+              key={index}
+              onClick={() => handleClick(index)}
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={() => handleMouseLeave(index)}
+              size={60}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
