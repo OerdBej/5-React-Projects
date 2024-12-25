b7186f171d8264e86f02cb59dfe7b8013aaa8af8
oerd bej <oerdbej@gmail.com>
2024-12-25 22:40:28 +0100

    fetching data images - ITS ALIVEEE

--- LoadMeMoreBaby/LoadMoreBaby/src/component/LoadMoreData.jsx
+++ LoadMeMoreBaby/LoadMoreBaby/src/component/LoadMoreData.jsx
@@ -10,13 +10,14 @@ const LoadMoreData = () => {
     try {
       const response = await fetch(
         `https://dummyjson.com/products?limit=20&skip=${
-          counter === 0 ? 0 : counter * 20
+          counter === 0 ? 0 : counter * 20 //button clicked? increase dataProduct index to 1 => so skip 20.
         }`
       );
       const result = await response.json();
       console.log(result);
-      if (result && result.product && result.product.length) {
-        setDataProduct(result.product);
+      //🔴 increasing the array if we have a response and if the response contains a product that is greater than 0
+      if (result && result.products && result.products.length) {
+        setDataProduct(result.products);
       }
       setLoading(false);
     } catch (error) {
@@ -29,10 +30,25 @@ const LoadMoreData = () => {
     fetchProducts();
   }, []);
 
+  //🔴 if loading data is not true
   if (loading) {
     return <div>blah loading data</div>;
   }
-  return <div className='container'>LoadMoreData</div>;
+  return (
+    <div className='container'>
+      <div>
+        {dataProduct && dataProduct.length
+          ? dataProduct.map((item) => (
+              <div key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
+      <button>Load me more</button>
+    </div>
+  );
 };
 
 export default LoadMoreData;
